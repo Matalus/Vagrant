a896bc6321644ee5447b4e15eea2d3c9be912b80
matth <matt.hamende@gmail.com>
2018-10-27 21:02:27 -0700

    updates to 2012 provisioning

---
+++ serv2012R2-PS5/TrustedHost_Patch.ps1
@@ -0,0 +1,17 @@
+﻿$list = @(
+    "MATTDESKTOP"
+)
+
+$trustedhosts = Get-ChildItem WSMan:\localhost\Client\TrustedHosts | Select-Object Value
+$trustedhosts
+ForEach ($server in $list) {
+  $server = $server.ToString()
+  "to be added $server"
+  $trustedhosts = $trustedhosts.ToString()
+  $trustedhosts = ($server + ",$trustedhosts").TrimEnd(",")
+  "list $trustedhosts"
+}
+""
+set-item wsman:\localhost\Client\TrustedHosts -value $trustedhosts
+
+Get-WSManInstance -ResourceURI winrm/config/client | Format-Table TrustedHosts -AutoSize -Wrap
---
+++ serv2012R2-PS5/shell/InstallChocoPackages.bat
@@ -0,0 +1,5 @@
+chocolatey feature enable -n=allowGlobalConfirmation
+choco install jre8
+choco install nodejs 
+choco install visualstudiocode 
+chocolatey feature disable -n=allowGlobalConfirmation
