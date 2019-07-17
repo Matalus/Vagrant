a5be23cd600b15328224baf98ce32c73248d67aa
Matt Hamende <matt.hamende@gmail.com>
2019-07-17 02:03:21 -0700

    comment out non-essential packages

--- serv2012R2-PS5/shell/InstallChocoPackages.bat
+++ serv2012R2-PS5/shell/InstallChocoPackages.bat
@@ -1,18 +1,22 @@
 REM bootstrap script to call chocolatey to install multiple packages
+REM add comments to packages you don't need
 
 REM disable confirmations for package install
 chocolatey feature enable -n=allowGlobalConfirmation
 
-choco install powershell-core
+choco install dotnet4.7.2
+choco install dotnetcore-windowshosting
+choco install dotnetcore
 choco install powershell
-choco install nodejs 
-choco install vscode 
-choco install vscode-powershell
+:: choco install jre8
+:: choco install nodejs 
+:: choco install vscode 
+:: choco install vscode-powershell
 choco install notepadplusplus
 choco install googlechrome
-choco install git
-choco install putty
-choco install awstools.powershell
+:: choco install git
+:: choco install putty
+:: choco install awstools.powershell
 
 REM Reenable confirmations
 chocolatey feature disable -n=allowGlobalConfirmation
