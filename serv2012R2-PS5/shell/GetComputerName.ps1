ad9b533e7c2097594c5aca9e18cf5eefb02300b3
Matt Hamende <matt_hamende@uhaul.com>
2019-07-16 23:42:13 -0700

    Remove untracked

--- serv2012R2-PS5/shell/GetComputerName.ps1
+++
@@ -1,6 +0,0 @@
-#Get New Computer Name
-$ComputerName = $Env:COMPUTERNAME
-$SID = (Get-WmiObject Win32_UserAccount)[0].SID
-
-Write-Host -ForegroundColor White "ComputerName : $($ComputerName) : SID $($SID)"
-
