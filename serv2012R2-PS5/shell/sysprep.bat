4dea4165a5a6dd5643a5f0b50e59e49851a26ce6
Matt Hamende <matt_hamende@uhaul.com>
2019-07-17 00:04:41 -0700

    remove untracked

--- serv2012R2-PS5/shell/sysprep.bat
+++
@@ -1,5 +0,0 @@
-REM Runs Sysprep and generalizes machine
-REM $sysprep_path = "C:\windows\system32\sysprep\sysprep.exe"
-REM $argumentlist = "/Generalize /OOBE /mode:vm /quiet /restart"
-
-c:\windows\system32\sysprep\sysprep.exe /generalize /oobe /mode:vm /quiet
